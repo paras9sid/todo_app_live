eb07acfea12e0992ce59e796504138b7ff3e0966
Sidharth Jain <sidharthjainp9@gmail.com>
2024-08-07 13:06:30 +0530

    Deployment settings changed

--- todo_main/settings.py
+++ todo_main/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = 'django-insecure-_!wo=2vkq=@al*!&1sypi4y2+*^5&ww2rf5_-aizhvmd-ky4h9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
